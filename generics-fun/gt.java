342a42277354269d092d1ed184fef379fd28348b
YooWaan <rterunuma@gmail.com>
2021-02-06 15:52:38 +0900

    updpate java test case

--- generics-fun/gt.java
+++ generics-fun/gt.java
@@ -1,32 +1,118 @@
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.DisplayName;
 
 
 @DisplayName("test::g.class")
 public class gt {
 
+    void p(String s) { System.out.println(s);}
+
+    void print(List<?> l) {
+        System.out.println(
+        l.stream().map(v -> v.toString())
+        .reduce("", (t, e) -> t.length() == 0 ? e :t + "," + e )
+        );
+    }
+
     @Test
-    void check() {
-
-        System.out.println("test example");
-        assertEquals(3, Integer.valueOf(3));
-
-        ValueInt i = new ValueInt(100);
-        assertEquals("100", i.toString());
-    }
-
-
+    @Tag("invarient")
     void confirmInvarient() {
+		ValueObject vo = new ValueObject(Boolean.TRUE);
+		ValueStr vs = new ValueStr("Hello");
+        ValueInt vi = new ValueInt(Integer.valueOf(123));
+        
+        List<ValueInt> list = new ArrayList<>();
+        list.add(vi);
+        //list.add(vs); //compile error
+        //list.add(vo); //compile error
+        //List<ValueInt> alist = Arrays.asList(vi, vs, vo); // compile error
+
+        // Covarient?
+        List<ValueObject> olst = new ArrayList<>();
+        // List<ValueObject> ol = new ArrayList<ValueStr>();
+        // type is invarient
+        olst.add(vi);
+        olst.add(vs);
+        olst.add(vo);
+
+        assertEquals(1, list.size());
+        assertEquals(3, olst.size());
+
+        p("invarient....");
+        print(list);
+        print(olst);
     }
 
-
+    @Test
+    @Tag("covarient")
     void confirmCovarient() {
+		ValueObject vo = new ValueObject(Boolean.TRUE);
+		ValueStr vs = new ValueStr("Hello");
+        ValueInt vi = new ValueInt(Integer.valueOf(123));
+
+        // create mutable list
+        List<ValueObject> all = new ArrayList<>(Arrays.asList(vo, vs, vi));
+        List<ValueObject> intstr = new ArrayList<>(Arrays.asList(vs, vi));
+        List<ValueStr> two = new ArrayList<>(Arrays.asList(vs, vi));
+
+        List<? extends ValueStr> list = two;
+        //List<? extends ValueStr> list = all,intstr;
+        //list.add(vi); // compile error
+        //list.add(vs); // compile error
+        //list.add(vo); // compile error
+
+        p("covarient....");
+        print(list);
+
+        assertEquals(2, list.size());
+        // can get item at index
+        assertEquals("Hello", list.get(0).strValue());
+        assertEquals("123", list.get(1).strValue());
+
+        List<ValueInt> num = Arrays.asList(vi, vi);
+        // num can not add ValueStr,Object
+        list = num; //List<? extends ValueStr> 
+        //list.add(vs); // compile error
+        //list.add(vo); // compile error
+
+        print(list);
+        assertEquals(2, list.size());
+        // can get item at index
+        assertEquals("123", list.get(0).strValue());
+        assertEquals("123", list.get(1).strValue());
     }
 
+    @Test
+    @Tag("contravarient")
     void confirmContravarient() {
+        ValueObject vo = new ValueObject(Boolean.TRUE);
+		ValueStr vs = new ValueStr("Hello");
+        ValueInt vi = new ValueInt(Integer.valueOf(123));
+ 
+        List<Object> all = new ArrayList<>(Arrays.asList(vo, vs, vi));
+
+        List<? super ValueStr> vlst = all;
+        //vlst.add(vo); // compile error
+        vlst.add(vi);
+        vlst.add(vs);
+
+        p("contravarient....");
+        print(vlst);
+
+        List<? super Object> list = all;
+        // become type unsafe list
+        list.add(Integer.valueOf(111));
+        list.add(Boolean.FALSE);
+        list.add(List.class);
+
+        print(list);
     }
 
 }
